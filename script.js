6ffb37b440f535cbe25d64f0ea72dc3ff37b7aeb
biotoscano <biotoscano@gmail.com>
2023-02-03 00:47:47 +0100

    bigger tuna

--- script.js
+++ script.js
@@ -23,12 +23,48 @@ window.onmousemove = e => {
         const xDiff = xDecimal - centerX;
         const yDiff = yDecimal - centerY;
 
-        const angle = Math.atan(yDiff/xDiff) + 'rad';
+        const angle = Math.atan(yDiff / xDiff) + 'rad';
         x.style.transform += `rotate(${angle})`;
 
-        if (xDiff <= 0){
+        if (xDiff <= 0) {
             x.style.transform += 'scaleX(-1)'
         }
 
     })
 }
+
+// window.ontouchmove = e => {
+//     // setTimeout(e => {
+
+//         const mouseX = e.touches[0].clientX;
+//         const mouseY = e.touches[0].clientY;
+
+//         const xDecimal = mouseX / window.innerWidth;
+//         const xDecimalFactor = (xDecimal * 2) - 1;
+//         const yDecimal = mouseY / window.innerHeight;
+//         const yDecimalFactor = (yDecimal * 2) - 1;
+
+//         atunes.forEach(x => {
+//             x.style.transform = `translateX(${-xDecimalFactor * 30}%)`;
+//             x.style.transform += `translateY(${-yDecimalFactor * 30}%)`;
+
+//             const { left, top, width, height } = x.getBoundingClientRect();
+//             let centerX = left + width / 2;
+//             centerX = centerX / window.innerWidth;
+//             let centerY = top + height / 2;
+//             centerY = centerY / window.innerHeight;
+
+//             const xDiff = xDecimal - centerX;
+//             const yDiff = yDecimal - centerY;
+
+//             const angle = Math.atan(yDiff / xDiff) + 'rad';
+//             x.style.transform += `rotate(${angle})`;
+
+//             if (xDiff <= 0) {
+//                 x.style.transform += 'scaleX(-1)'
+//             }
+
+//         })
+//     // }, 20)
+
+// }
